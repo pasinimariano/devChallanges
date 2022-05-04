d63e8b59c9603f1b14619f04bfecb4283e9a8157
pasini_mariano <pasinimariano@gmail.com>
2022-05-04 16:24:01 -0300

    Table creator with SqlAlchemy, and set relationships

--- my-unsplash/server/src/db/index.py
+++ my-unsplash/server/src/db/index.py
@@ -1,11 +1,8 @@
-import uuid
-import sqlalchemy
-from datetime import datetime
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy import Table, Column, String, DateTime
-from sqlalchemy_utils import database_exists, create_database, UUIDType
+from sqlalchemy_utils import database_exists, create_database
 
 from dotenv import dotenv_values
+from .tables import create_all_tables
 
 ENV = dotenv_values()
 DB_DIALECT = ENV['DB_DIALECT']
@@ -40,18 +37,8 @@ class DbConnection:
                 print('Error when trying to create the database: {}'.format(error))
 
     def create_tables(self):
-        if not sqlalchemy.inspect(self.engine).has_table('Users'):
-            try:
-                Table('Users', self.metadata,
-                      Column('id', UUIDType(binary=False), primary_key=True, default=uuid.uuid4()),
-                      Column('firstname', String(50), nullable=False),
-                      Column('lastname', String(50), nullable=False),
-                      Column('email', String(50), nullable=False, unique=True),
-                      Column('created_at', DateTime(), default=datetime.now())
-                      )
+        try:
+            create_all_tables(self.engine)
 
-                self.metadata.create_all()
-                print('Tables was successfully created')
-
-            except Exception as error:
-                print('Error when trying to create the tables: {}'.format(error))
+        except Exception as error:
+            print('Error when trying to create the tables: {}'.format(error))
---
+++ my-unsplash/server/src/db/tables.py
@@ -0,0 +1,33 @@
+import uuid
+from datetime import datetime
+from sqlalchemy import Column, Text, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy_utils import UUIDType
+
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4())
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
+    password = Column(String(128), nullable=False)
+    created_at = Column(DateTime(), default=datetime.now())
+    images = relationship('Images', backref='owner')
+
+
+class Images(Base):
+    __tablename__ = 'images'
+    id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4())
+    image_file = Column(Text, nullable=False)
+    date_posted = Column(DateTime(), default=datetime.now())
+    owner_id = Column(UUIDType(binary=False), ForeignKey('users.id'))
+
+
+def create_all_tables(engine):
+    try:
+        Base.metadata.create_all(engine)
+    except Exception as error:
+        print('Error when trying to create the tables: {}'.format(error))
