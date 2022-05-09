dbebfaf29c4e24d41132fd9da4ef0a178ad38482
pasini_mariano <pasinimariano@gmail.com>
2022-05-09 19:45:37 -0300

    Delete my-unsplash/server/env directory

--- my-unsplash/server/env/lib/python3.8/site-packages/authomatic/providers/persona.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from authomatic import providers
-
-
-class MozillaPersona(providers.AuthenticationProvider):
-    pass
--- my-unsplash/server/env/lib/python3.8/site-packages/pyramid_mailer/testing.py
+++
@@ -1,7 +0,0 @@
-from pyramid_mailer import _set_mailer
-from pyramid_mailer.mailer import DummyMailer
-
-
-def includeme(config):
-    mailer = DummyMailer()
-    _set_mailer(config, mailer)
